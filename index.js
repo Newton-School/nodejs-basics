1bd7a6232c27ce64460cb76251c75fa7a05cfba4
Rahul Shetty <rahul.shetty@here.com>
2020-07-03 22:31:49 +0530

    Add message to file

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
 const http = require("http");
+const fs = require("fs");
 
 const reqListener = (req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const { url, method } = req;
+  res.setHeader("Content-Type", "text/html");
 
-  res.write("Hello, node!");
-  res.end();
+  if (url === "/") {
+    res.write(`
+      <html>
+        <head><title>Learn Nodejs</title></head>
+
+        <body>
+          <h1>Hello, Node!</h1>
+
+          <form action="/message" method="POST">
+            <input type="text" placeholder="Add your message" name="message" />
+          </form>
+        </body>
+      </html>
+    `);
+
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunkOfData) => {
+      body.push(chunkOfData);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFile("message.txt", message, (err) => {
+        if (err) {
+          res.send(err);
+        }
+
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+
+        return res.end();
+      });
+    });
+  }
+
+  res.write(`
+    <html>
+      <head><title>Learn Nodejs</title></head>
+
+      <body>
+        <h1>Hello, Node!</h1>
+
+        <p>404: Not Found</p>
+      </body>
+    </html>
+  `);
+
+  return res.end();
 };
 
 const server = http.createServer(reqListener);
